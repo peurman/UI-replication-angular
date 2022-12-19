0d022e887c49214164ccb214b1dcb4aa8f6f68a7
Esteban Manrupe <esteban_manrupe@hotmail.com>
2022-12-18 23:38:55 -0300

    radarChart layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HeaderComponent],
+  declarations: [AppComponent, NavbarComponent, HeaderComponent, RadarChartComponent],
   imports: [BrowserModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/radar-chart/radar-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radar-chart',
+  templateUrl: './radar-chart.component.html',
+  styleUrls: ['./radar-chart.component.scss'],
+})
+export class RadarChartComponent {
+  personal = 34;
+  team = 22;
+  department = 23;
+}
